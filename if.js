2fe066a0e3e6df6a3df85c2ab1e24e54b9244850
saifmahin500 <saifmahin500@gmail.com>
2025-06-18 18:24:16 +0600

    first commit

---
+++ if.js
@@ -0,0 +1,18 @@
+if (3 < 10 ) {
+    // console.log("chotoo")
+}
+
+var apple = 500;
+
+if(apple < 600){
+    // console.log("mama give me some money")
+}
+
+var weight = 40;
+
+if ( weight < 20) {
+    console.log('i will carry it by myself')
+}
+else{
+    console.log('i will rent a richsaw')
+}
---
+++ variable.js
@@ -0,0 +1,38 @@
+//problem - 1
+
+var orangePrice = 250;
+var applePrize = 150;
+var totalPrice = (orangePrice + applePrize);
+
+var mainAmount = 1000;
+var back = (mainAmount - totalPrice);
+
+
+
+// console.log(back);
+
+
+
+//problem - 2
+
+var math = 80;
+var eng = 76.35;
+var bangla = 65.88;
+
+var totalMark = (math + eng + bangla);
+var avgMark = (totalMark / 3);
+
+
+
+// console.log(totalMark);
+// console.log(avgMark.toFixed(2));
+
+
+// problem -- 3
+
+var num1 = 119;
+var num2 = 5;
+
+
+var total = (num1 % num2);
+console.log(total);
