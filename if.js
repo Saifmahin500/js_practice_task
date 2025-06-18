4923ad237848f14081529991f22dd95bc68fec94
saifmahin500 <saifmahin500@gmail.com>
2025-06-18 18:37:26 +0600

    hello

--- if.js
+++ if.js
@@ -8,11 +8,29 @@ if(apple < 600){
     // console.log("mama give me some money")
 }
 
-var weight = 40;
+// var weight = 40;
 
-if ( weight < 20) {
-    console.log('i will carry it by myself')
-}
-else{
-    console.log('i will rent a richsaw')
-}
+// if ( weight < 20) {
+//     console.log('i will carry it by myself')
+// }
+// else{
+//     console.log('i will rent a richsaw')
+// }
+
+const salary = 30000;
+const iaBcs = true;
+const height = 61;
+
+// if (salary > 25000 && height > 66 ) {
+// console.log('good salary')
+// }
+// else{
+//     console.log("bad salary")
+// }
+
+if (salary > 25000 || height > 66 ) {
+    console.log('good salary')
+    }
+    else{
+        console.log("bad salary")
+    }
