395ab18af3f8c175236cad44043fd3ae70f8fc4b
saifmahin500 <saifmahin500@gmail.com>
2025-06-22 12:17:35 +0600

    registration

--- if.js
+++ if.js
@@ -17,9 +17,9 @@ if(apple < 600){
 //     console.log('i will rent a richsaw')
 // }
 
-const salary = 30000;
-const iaBcs = true;
-const height = 61;
+// const salary = 30000;
+// const iaBcs = true;
+// const height = 61;
 
 // if (salary > 25000 && height > 66 ) {
 // console.log('good salary')
@@ -28,9 +28,36 @@ const height = 61;
 //     console.log("bad salary")
 // }
 
-if (salary > 25000 || height > 66 ) {
-    console.log('good salary')
-    }
-    else{
-        console.log("bad salary")
-    }
+// if (salary > 25000 || height > 66 ) {
+//     console.log('good salary')
+//     }
+//     else{
+//         console.log("bad salary")
+//     }
+
+
+// const price = 2000;
+
+// if (price >= 5000) {
+//     const discount = price * 10 / 100;
+//     const payAmount = price - discount;
+//     console.log(payAmount);
+    
+// }
+// else if(price > 2500){
+//     const discount = price * 5 / 100;
+//     const payAmount = price - discount;
+//     console.log(payAmount);
+// }
+// else{
+//     console.log(price);
+// }
+
+const age = 17;
+
+if (age <= 12) {
+    console.log("free");
+}
+else if(age >= 60) {
+    console.log("50% dis");
+}
