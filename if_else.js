c56772b36c96df5cb49bb6229ba6ae4ed2ec553c
saifmahin500 <saifmahin500@gmail.com>
2025-06-25 11:48:34 +0600

    js file

---
+++ if_else.js
@@ -0,0 +1,18 @@
+const money = 0;
+
+if (money > 300) {
+    console.log("bro you are rich");
+}
+else{
+    if(money > 100){
+        console.log("tumi gorib ow na borolok ow na");
+    }
+    else{
+        if (money > 0) {
+            console.log("dosto kola kha");
+        }
+        else{
+            console.log("bye bye dosto");
+        }
+    }
+}
---
+++ ternary.js
@@ -0,0 +1,9 @@
+const age = 21;
+// if (age >= 18 ) {
+//     console.log("you can vote");
+// }
+// else{
+//     console.log("ghumay thako");
+// }
+
+age >= 18 ? console.log("you are adult") : console.log("bacha");
